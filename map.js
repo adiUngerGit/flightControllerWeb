07c88a0fb02469c181f9f7618051f3d3fe0ed2e9
adiUngerGit <58906086+adiUngerGit@users.noreply.github.com>
2020-07-19 17:50:17 +0300

    Add files via upload

---
+++ map.js
@@ -0,0 +1,9 @@
+﻿"use strict";
+function load() {
+
+    let options = {
+        zoom: 8,
+        center: { lat: 31.771959, lng: 35.217018 }
+    }
+    var map = new google.maps.Map(document.getElementById('map'), options);
+}
